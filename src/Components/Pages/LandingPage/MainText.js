01fc46a506935e726dcb17b9a2eb71b6bc9dfcab
Stephen George <stephenngeorge@gmail.com>
2019-03-05 15:13:18 +0000

    main text component

---
+++ src/Components/Pages/LandingPage/MainText.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default () => (
+    <>
+        <h1>graphic.js</h1>
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+            Eos, deserunt laborum expedita possimus quae, iste saepe, 
+            cum perferendis ratione adipisci vel in consequuntur odio 
+            officiis error! Excepturi, eaque incidunt. Omnis.
+        </p>
+    </>
+)
--- src/Components/Pages/LandingPage/index.js
+++ src/Components/Pages/LandingPage/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
 // import child components
-import { RandomWalker } from '../../Canvases'
+import MainText from './MainText'
 
 export default () => (
     <>
-        <RandomWalker />
+        <MainText />
     </>
 )
